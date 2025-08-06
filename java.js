c417f4657c28006ffcb0c92dadfabb8821e2c2f4
Tomas Levy <tomaslevy1999@gmail.com>
2025-08-06 12:36:50 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,9 +1,7 @@
 const no_button=document.getElementById('no');
 const yes_button = document.getElementById('yes');
-const close_button = document.querySelectorAll('.close');
 const contenedor = document.getElementById('contenedor');
-const mensajePositivo = document.getElementById('mensaje-positivo');
-const mensajeNegativo = document.getElementById('mensaje-negativo');
+const mensaje = document.getElementById('mensaje');
 
 let cambiarLugar = function(){
     randomNumber = function(max){
@@ -20,37 +18,12 @@ let cambiarLugar = function(){
     no_button.style.position = 'absolute';
 };
 
-// no_button.addEventListener('mouseover',cambiarLugar);
-no_button.addEventListener('click',function(e){
-    e.preventDefault();
-    console.log('funcionooo')
-    mensajeNegativo.style.visibility= 'visible';
-    contenedor.style.visibility='hidden';
-    document.body.style.backgroundColor = 'red';
-    mensajeNegativo.style.backgroundColor = 'white';
-});
+no_button.addEventListener('mouseover',cambiarLugar);
 yes_button.addEventListener('click',function(e){
     e.preventDefault();
     console.log('funcionooo')
-    mensajePositivo.style.visibility= 'visible';
+    mensaje.style.visibility= 'visible';
     contenedor.style.visibility='hidden';
     document.body.style.backgroundColor = 'red';
-    mensajePositivo.style.backgroundColor = 'white';
+    mensaje.style.backgroundColor = 'white';
 });
-
-   close_button.forEach(element => {
-
-       element.addEventListener('click',function(e){
-    e.preventDefault();
-    console.log('funcionooo')
-    mensajePositivo.style.visibility= 'hidden';
-    mensajeNegativo.style.visibility= 'hidden';
-    contenedor.style.visibility='visible';
-    document.body.style.backgroundColor = 'white';
-
-});
-    
-   });
-
-
-
